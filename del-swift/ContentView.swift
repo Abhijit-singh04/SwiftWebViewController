48181fef9b267a3b6fa9d55353e526c2ead1045e
Abhijit-singh04 <2001.abhijitsingh@gmail.com>
2023-04-18 10:51:53 +0530

    Initial Commit

---
+++ del-swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  del-swift
+//
+//  Created by Abhijit singh on 18/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ del-swift/del_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  del_swiftApp.swift
+//  del-swift
+//
+//  Created by Abhijit singh on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct del_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
