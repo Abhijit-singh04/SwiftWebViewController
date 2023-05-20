c406a9379a5dd72def7205bed03f44a28aa0d165
Abhijit-singh04 <2001.abhijitsingh@gmail.com>
2023-05-20 10:11:09 +0530

    First Commit

--- del-swift/ContentView.swift
+++ del-swift/ContentView.swift
@@ -1,26 +1,33 @@
-//
-//  ContentView.swift
-//  del-swift
-//
-//  Created by Abhijit singh on 18/04/23.
-//
+import UIKit
+import WebKit
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+class ViewController: UIViewController {
+    
+    var webViewArray = [WKWebView]()
+    var homeWebView: WKWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create and add home web view to the view hierarchy
+        let homeConfig = WKWebViewConfiguration()
+        homeWebView = WKWebView(frame: view.frame, configuration: homeConfig)
+        view.addSubview(homeWebView!)
+        
+        // Create and add button to the view hierarchy
+        let button = UIButton(frame: CGRect(x: view.frame.midX - 50, y: view.frame.midY - 25, width: 100, height: 50))
+        button.setTitle("Create Web View", for: .normal)
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(createWebView), for: .touchUpInside)
+        view.addSubview(button)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    @objc func createWebView() {
+        // Create new web view and add to the view hierarchy
+        let webViewConfig = WKWebViewConfiguration()
+        let newWebView = WKWebView(frame: view.frame, configuration: webViewConfig)
+        view.addSubview(newWebView)
+        webViewArray.append(newWebView)
     }
+    
 }
